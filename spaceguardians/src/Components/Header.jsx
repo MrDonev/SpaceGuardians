e02a72f466855f18ffd21125f3204c8fc2a5b54d
Iliya <mrdonev@gmail.com>
2022-06-24 15:03:08 +0100

    basic setup for phaser game into a react front end page

---
+++ spaceguardians/src/Components/Header.jsx
@@ -0,0 +1,4 @@
+const Header = ()=>{
+    return <header><h1>Header</h1></header>
+}
+export default Header;
---
+++ spaceguardians/src/Components/UserProfile.jsx
@@ -0,0 +1,6 @@
+
+const UserProfile=()=>{
+    return <h2>User Profile section</h2>
+}
+
+export default UserProfile;
