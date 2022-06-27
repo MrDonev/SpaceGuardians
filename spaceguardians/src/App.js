ce2a56699fd5ca0c30ed6aaa1ab8a4d186b73061
baurzhaan <baur007@gmail.com>
2022-06-27 11:32:02 +0100

    email/password authentication

--- spaceguardians/src/App.js
+++ spaceguardians/src/App.js
@@ -2,13 +2,36 @@ import GameComponent from './Game';
 import './App.css';
 import Header from './Components/Header';
 import UserProfile from './Components/UserProfile';
+
+import React from 'react';
+import Signin from './Components/SignIn';
+import Signup from './Components/SignUp';
+import Account from './Components/Account';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './Components/ProtectedRoute';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
       <main id="main">
         <div id="game">
-          <GameComponent />
+          {/* <GameComponent /> */}
         </div>
         <UserProfile />
       </main>
@@ -17,3 +40,31 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className='text-center text-3xl font-bold'>
+//         Arcadian Game
+//       </h1>
+//       <AuthContextProvider>
+//         <Routes>
+//           <Route path='/' element={<Signin />} />
+//           <Route path='/signup' element={<Signup />} />
+//           <Route
+//             path='/account'
+//             element={
+//               <ProtectedRoute>
+//                 <Account />
+//               </ProtectedRoute>
+//             }
+//           />
+//         </Routes>
+//       </AuthContextProvider>
+//     </div>
+//   );
+// }
+// export default App;
