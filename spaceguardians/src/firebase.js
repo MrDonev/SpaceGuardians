f175cbe5b3fa06050c6008ce2bb1bddb22a55a47
jomit1612 <jomitthomas1612@gmail.com>
2022-06-28 11:42:26 +0100

    add data and read data from database sorted

--- spaceguardians/src/firebase.js
+++ spaceguardians/src/firebase.js
@@ -3,9 +3,10 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
-  signOut,
+  // signOut,
   signInAnonymously,
 } from "firebase/auth";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfU9SJ5JQ4m47DI_wtZiG-xdFHlZ73rvE",
@@ -14,6 +15,8 @@ const firebaseConfig = {
   storageBucket: "spaceguardians-d5924.appspot.com",
   messagingSenderId: "27183731871",
   appId: "1:27183731871:web:655c6b31483b83d9f37eaa",
+  databaseURL:
+    "https://spaceguardians-d5924-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
@@ -55,6 +58,36 @@ export const anonymousSignIn = () => {
       // ...
     });
 };
+
+function writeUserData(
+  userId,
+  name,
+  email,
+  score,
+  level,
+
+  url,
+  position
+) {
+  const db = getDatabase(app);
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    highscore: score,
+    level: level,
+
+    avatar: url,
+    scoreboardPosition: position,
+  });
+  const userRef = ref(db, "users/" + userId + "/email");
+  onValue(userRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
+  });
+}
+
+writeUserData("andy", "awu", "awu@gmail.com", 25, 34, "sfsdfs", 12);
+writeUserData("Tom", "awu", "awu@gmail.com", 25, 34, "sfsdfs", 9);
 
 // export const logout = () => {
 //   auth.signOut()
