8d1d0562b4020800ee542660dc6f8c8679fc6174
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-06-28 12:53:47 +0100

    issues with physics and enemy movement

--- spaceguardians/src/Scenes/GameScene.js
+++ spaceguardians/src/Scenes/GameScene.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser, { Tilemaps } from 'phaser';
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -16,6 +16,13 @@ class GameScene extends Phaser.Scene {
     this.load.image('strongestInvader', '../assets/strongestEnemy.png');
   }
   create() {
+    console.log(this.game);
+
+
+    //this.world.setBounds(0,0, 800, 600);
+
+    //this.player.body.collideWorldBounds = true;
+
     this.starfield = this.add.image(0, 0, 'starfield').setScale(3);
     this.player = this.add.image(400, 530, 'player');
     //creating the aliens
@@ -23,48 +30,62 @@ class GameScene extends Phaser.Scene {
     this.aliens.enableBody = true;
     this.aliens.physicsBodyType = Phaser.Physics.ARCADE;
     this.createAliens();
+
+    this.physics.arcade(this.player, true);
+
   }
   createAliens() {
     for (let y = 4; y < 7; y++) {
       for (let x = 4; x < 14; x++) {
-        let alien = this.aliens.create(x * 48, y * 35, 'blueInvader');
-        // alien.anchor.setTo(1, 1);
-        // alien.animations.add('fly', [0, 1, 2, 3], 20, true);
-        // alien.play('fly');
-        // alien.body.moves = false;
+        this.alien = this.aliens.create(x * 48, y * 35, 'blueInvader');
+      
       }
     }
     for (let y = 3; y < 4; y++) {
       for (let x = 5; x < 13; x++) {
-        let alien = this.aliens.create(x * 48, y * 35, 'blueYellowInvader');
+        this.alien = this.aliens.create(x * 48, y * 35, 'blueYellowInvader');
       }
     }
     for (let y = 2; y < 3; y++) {
       for (let x = 6; x <= 11; x++) {
-        let alien = this.aliens.create(x * 48, y * 35, 'redBlueInvader');
+        this.alien = this.aliens.create(x * 48, y * 35, 'redBlueInvader');
       }
     }
     for (let y = 1; y < 2; y++) {
       for (let x = 7; x <= 10; x++) {
-        if (x === 7 || x == 10) {
-          let alien = this.aliens.create(x * 48, y * 35, 'strongestInvader');
+        if (x === 7 || x === 10) {
+          this.alien = this.aliens.create(x * 48, y * 35, 'strongestInvader');
         }
       }
+
     }
-    this.aliens.x = 150;
-    this.aliens.y = 40;
+     this.aliens.children.iterate((child) => {
+      child.x += 5
+     })
 
-    // //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    // var tween = this.add
-    //   .tween(this.aliens)
-    //   .to({ x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-    //  When the tween loops it calls descend
-    // tween.onLoop.add(this.descend, this);
+     
   }
+
+
+
   descend() {
     this.aliens.y += 10;
+  }
+
+  update() {
+    const cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown){
+      this.player.x -= 3;
+    }
+    if (cursors.right.isDown){
+      this.player.x += 3;
+    }
+
+
   }
 }
 
 export default GameScene;
+
+
+//game.add.tween(gameTitle).to({x: 300}, 2000, Phaser.Easing.Cubic.InOut, true, 0);
