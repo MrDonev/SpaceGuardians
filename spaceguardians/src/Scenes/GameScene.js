d6c80013b4226906b0b045afcf71d379f81c1a07
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-06-28 17:10:22 +0100

    animation and player controls working

--- spaceguardians/src/Scenes/GameScene.js
+++ spaceguardians/src/Scenes/GameScene.js
@@ -1,4 +1,4 @@
-import Phaser, { Tilemaps } from 'phaser';
+import Phaser from 'phaser';
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -21,14 +21,19 @@ class GameScene extends Phaser.Scene {
     this.starfield = this.add.image(0, 0, 'starfield').setScale(3);
     this.player = this.physics.add.image(400, 530, 'player');
     this.player.setCollideWorldBounds(true)
+
     //creating the aliens
-    this.aliens = this.add.group();
+    this.aliens = this.physics.add.group();
     this.aliens.enableBody = true;
     this.aliens.physicsBodyType = Phaser.Physics.ARCADE;
+    this.container = this.add.container(0, 0);
+    //this.container.setCollideWorldBounds(true);
     this.createAliens();
 
   }
   createAliens() {
+
+
     for (let y = 4; y < 7; y++) {
       for (let x = 4; x < 14; x++) {
         this.alien = this.aliens.create(x * 48, y * 35, 'blueInvader');
@@ -51,14 +56,38 @@ class GameScene extends Phaser.Scene {
         }
       }
     }
-    // this.aliens.children.iterate((child) => {
-    //   child.x += 5;
-    // });
+       this.container.add(this.aliens.children.entries)
+
+       var destX = -10;
+
+    var tween = this.tweens.add({
+        targets: this.container,
+        duration: 6000,
+        yoyo: true,
+        repeat: -1,
+
+        x: {
+
+            getEnd: function (target, key, value)
+            {
+
+                return destX -155;
+            },
+
+            getStart: function (target, key, value)
+            {
+                return destX + 155;
+            }
+
+        }
+
+    });
+
+       
+
+   
   }
 
-  descend() {
-    this.aliens.y += 10;
-  }
 
   update() {
     const cursors = this.input.keyboard.createCursorKeys();
@@ -68,9 +97,11 @@ class GameScene extends Phaser.Scene {
     if (cursors.right.isDown) {
       this.player.x += 3;
     }
+    if (cursors.down.isDown) {
+      this.container.x += 5;
   }
 }
-
+}
 export default GameScene;
 
 //game.add.tween(gameTitle).to({x: 300}, 2000, Phaser.Easing.Cubic.InOut, true, 0);
